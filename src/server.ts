22b82f9635eaa6e4e8594e06e8a7fcf8d0a137d7
Sergio Vazquez <sergio@incotechsystems.com>
2024-11-14 11:27:29 -0600

    Re organize code

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,38 @@
 import express from 'express';
 
+require('dotenv').config();
+
 const cors = require('cors');
+
 const server = express();
+
+const TelegramBot = require('node-telegram-bot-api');
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_BOT_TOKEN;
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg: { chat: { id: string } }, match: string[]) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg: { chat: { id: string } }) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
 
 server.use(cors());
 
